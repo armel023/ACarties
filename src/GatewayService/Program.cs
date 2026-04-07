cace9f41dd1ca5c58038293b9a90b1e4578db79a
Armel Ardael <ardael.armel@gmail.com>
2026-04-07 13:50:27 +0800

    Adding Notification Service and SignalR

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -13,9 +13,21 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.ValidateAudience = false;
         options.TokenValidationParameters.NameClaimType = "username";
     });
-    
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", policy =>
+    {
+        policy.AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials()
+              .WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors();
 app.MapReverseProxy();
 app.UseAuthentication();
 app.UseAuthorization();
